cdd1cb976351c745dba078f7d979699c037ca182
Tapan Manna <mannatapan588@gmail.com>
2025-09-24 00:07:39 +0530

    Add my new file

---
+++ demo-gradle/src/main/java/com/example/demo_gradle/EmployeeApi.java
@@ -0,0 +1,14 @@
+package com.example.demo_gradle;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeApi {
+    @GetMapping("/{empId}")
+    public String getEmployee(String empId){
+        return "Employee Found";
+    }
+}
